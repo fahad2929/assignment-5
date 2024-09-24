61afc166a1169bf2174ed29c90d9e0192da06a5d
MD.ZISHANURL FAHAD <mdzishanurlfahad22@gmail.com>
2024-09-25 00:26:11 +0600

    Card 1 addMoney Complete

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,29 +1,54 @@
+
+    //............card 1 .............
 document.getElementById('btn-add-money').addEventListener('click',function(event){
     event.preventDefault();
 
    const addMoney = getInputFiledValueById('input-add-money');
    const balance = getTextFieldValueById('account-balance');
-   const balance2 = getTextFieldValueById('main-balance');
+   const balance2 = getTextValueById('main-balance');
    const newBalance = balance + addMoney ;
    const bala = balance2 - newBalance;
 
+   if( addMoney> 0){
+    alert('Your Donation is Added')
+   }
+   else{
+    alert('Donation Added Failed');
+   }
    document.getElementById('main-balance').innerText = bala ;
    document.getElementById('account-balance').innerText = newBalance;
 
 });
 
-document.getElementById('btn-add-money2').addEventListener('click',function(event){
-    event.preventDefault();
+
+//         //  ...........card 2 .............
+// document.getElementById('btn-add-money2').addEventListener('click',function(event){
+//     event.preventDefault();
      
-    const addMoney = getInputValueById('input-add-money2');
-    const balance = getTextValueById('account-balance2');
-    const balance2 = getTextValueById('main-balance');
-    const newBalance = balance + addMoney ;
-    const ori = balance2 - newBalance;
-
-   document.getElementById('main-balance').innerText = ori ;
-   document.getElementById('account-balance2').innerText = newBalance;
+//     const addMoney1 = getInputValueById('input-add-money2');
+//     const balance1 = getTextValueById('account-balance2');
+//     const balance2 = getTextValueById('main-balance');
+//     const newBalance2 =  addMoney1 + balance1 ;
+//     const bala2 = balance2 - newBalance2;
 
 
+//    document.getElementById('main-balance').innerText = bala2 ;
+//    document.getElementById('account-balance2').innerText = newBalance2;
 
-})
+// })
+
+
+    //  // ..........card 3 ................
+    //  document.getElementById('btn-add-money3').addEventListener('click',function(){
+    //     event.preventDefault();
+         
+    //     const addMoney2 = getInput1ValueById('input-add-money3');
+    //     const balance2 = getText1ValueById('account-balance3');
+    //     const balance3 = getText1ValueById('main-balance');
+    //     const newBalance3 =  addMoney2 + balance2 ;
+    //     const bala3 = balance3 - newBalance3;
+    
+    //    document.getElementById('main-balance').innerText = bala3 ;
+    //    document.getElementById('account-balance3').innerText = newBalance3;
+    
+    // })
--- js/utilities.js
+++ js/utilities.js
@@ -1,6 +1,6 @@
     // common shared function here
 
-
+        //..................card 1...................
    function getInputFiledValueById(id){
     const inputValue = document.getElementById(id).value;
     const inputNumber = parseFloat(inputValue);
@@ -12,7 +12,7 @@
     const textNumber = parseFloat(textValue);
     return textNumber;}
 
-
+    //..............card 2..................
    function getInputValueById(id){
     const inputValue2 = document.getElementById(id).value;
     const inputNumber2 = parseFloat(inputValue2);
@@ -23,3 +23,16 @@
     const textValue2 = document.getElementById(id).innerText;
     const textNumber2 = parseFloat(textValue2);
     return textNumber2;}
+    // ...........card 3 .............
+
+    function getInput1ValueById(id){
+        const inputValue2 = document.getElementById(id).value;
+        const inputNumber2 = parseFloat(inputValue2);
+        return inputNumber2;
+       }
+    
+       function getText1ValueById(id){
+        const textValue2 = document.getElementById(id).innerText;
+        const textNumber2 = parseFloat(textValue2);
+        return textNumber2;}
+    
