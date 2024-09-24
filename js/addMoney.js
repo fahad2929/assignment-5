9bb2433f68b595965900e228d1355e934c0a565e
MD.ZISHANURL FAHAD <mdzishanurlfahad22@gmail.com>
2024-09-24 21:28:32 +0600

    added card 2

--- js/addMoney.js
+++ js/addMoney.js
@@ -1,18 +1,29 @@
 document.getElementById('btn-add-money').addEventListener('click',function(event){
     event.preventDefault();
 
-  const addMoney = getInputFiledValueById('input-add-money')
+   const addMoney = getInputFiledValueById('input-add-money');
+   const balance = getTextFieldValueById('account-balance');
+   const balance2 = getTextFieldValueById('main-balance');
+   const newBalance = balance + addMoney ;
+   const bala = balance2 - newBalance;
 
+   document.getElementById('main-balance').innerText = bala ;
+   document.getElementById('account-balance').innerText = newBalance;
 
-//    const balance = document.getElementById('account-balance').innerText;
-//    const bal = parseFloat(balance);
-
-//    const newBalance = bal + addMoney
-//    document.getElementById('account-balance').innerText = newBalance;
-
- 
-const balance = getTextFieldValueById('account-balance');
-const newBalance = balance + addMoney ;
-
-document.getElementById('account-balance').innerText = newBalance;
 });
+
+document.getElementById('btn-add-money2').addEventListener('click',function(event){
+    event.preventDefault();
+     
+    const addMoney = getInputValueById('input-add-money2');
+    const balance = getTextValueById('account-balance2');
+    const balance2 = getTextValueById('main-balance');
+    const newBalance = balance + addMoney ;
+    const ori = balance2 - newBalance;
+
+   document.getElementById('main-balance').innerText = ori ;
+   document.getElementById('account-balance2').innerText = newBalance;
+
+
+
+})
--- js/utilities.js
+++ js/utilities.js
@@ -1,5 +1,6 @@
     // common shared function here
 
+
    function getInputFiledValueById(id){
     const inputValue = document.getElementById(id).value;
     const inputNumber = parseFloat(inputValue);
@@ -9,5 +10,16 @@
    function getTextFieldValueById(id){
     const textValue = document.getElementById(id).innerText;
     const textNumber = parseFloat(textValue);
-    return textNumber;
+    return textNumber;}
+
+
+   function getInputValueById(id){
+    const inputValue2 = document.getElementById(id).value;
+    const inputNumber2 = parseFloat(inputValue2);
+    return inputNumber2;
    }
+
+   function getTextValueById(id){
+    const textValue2 = document.getElementById(id).innerText;
+    const textNumber2 = parseFloat(textValue2);
+    return textNumber2;}
